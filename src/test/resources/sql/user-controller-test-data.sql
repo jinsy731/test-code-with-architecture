e0fedf8e7aafc695508edc4a68ba592c19212f38
jinsy731 <jinsy731@gmail.com>
2023-07-03 15:55:24 +0900

    UserController 테스트 코드 작성

---
+++ src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,5 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values ('1', 'jinsy731@gmail.com', 'jinsy731', 'Seoul', 'aa-aaa-a-a', 'ACTIVE', 0);
+
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values ('2', 'jinsy732@gmail.com', 'jinsy732', 'Seoul', 'aa-aaa-a-aaa', 'PENDING', 0);
