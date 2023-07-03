d401d8f462f8a15505a34f93ae2a5bab921b1ecb
jinsy731 <jinsy731@gmail.com>
2023-07-03 14:18:29 +0900

    h2를 이용한 repository 테스트 추가

---
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest(showSql = true)
+@TestPropertySource("classpath:test-application.properties")
+@Sql("/sql/user-repository-test-data.sql")
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findByAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+        //then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+        //then
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("jinsy731@gmail.com", UserStatus.ACTIVE);
+        //then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        //given
+        //when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("jinsy731@gmail.com", UserStatus.PENDING);
+        //then
+        assertThat(result.isEmpty()).isTrue();
+    }
+}
---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values ('1', 'jinsy731@gmail.com', 'jinsy731', 'Seoul', 'aa-aaa-a-a', 'ACTIVE', 0);
