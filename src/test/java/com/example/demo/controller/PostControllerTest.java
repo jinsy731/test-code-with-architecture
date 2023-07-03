fb8b742865a7b83b898e3df4922989754cd60ca3
jinsy731 <jinsy731@gmail.com>
2023-07-03 16:20:49 +0900

    post controller 테스트 코드 작성

--- src/test/java/com/example/demo/controller/PostControllerTest.java
+++ src/test/java/com/example/demo/controller/PostControllerTest.java
@@ -1,28 +1,79 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.dto.PostUpdateDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/post-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD),
+})
 public class PostControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    void 헬스_체크_응답이_200으로_내려온다() throws Exception {
+    void 게시글_번호로_게시글을_조회할_수_있다() throws Exception {
         //given
-        mockMvc.perform(MockMvcRequestBuilders.get("/health_check.html"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
         //when
         //then
+        mockMvc.perform(get("/api/posts/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.content").value("hello world"))
+                .andExpect(jsonPath("$.writer.id").value(1))
+                .andExpect(jsonPath("$.writer.email").value("jinsy731@gmail.com"))
+                .andExpect(jsonPath("$.writer.nickname").value("jinsy731"));
     }
+
+    @Test
+    void 사용자가_존재하지_않는_게시글을_조회하려고_할_경우_에러가_발생한다() throws Exception {
+        //given
+        //when
+        //then
+        mockMvc.perform(get("/api/posts/123131"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Posts에서 ID 123131를 찾을 수 없습니다."));
+    }
+
+    @Test
+    void 사용자는_게시글을_수정할_수_있다() throws Exception {
+        //given
+        PostUpdateDto postUpdateDto = PostUpdateDto.builder()
+                .content("update post")
+                .build();
+        //when
+        //then
+        mockMvc.perform(put("/api/posts/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(postUpdateDto))
+            )
+                .andExpect(status().isOk())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.content").value("update post"))
+                .andExpect(jsonPath("$.writer.id").value(1))
+                .andExpect(jsonPath("$.writer.email").value("jinsy731@gmail.com"))
+                .andExpect(jsonPath("$.writer.nickname").value("jinsy731"));
+    }
+
 }
--- src/test/java/com/example/demo/controller/UserCreateControllerTest.java
+++ src/test/java/com/example/demo/controller/UserCreateControllerTest.java
@@ -1,28 +1,59 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.dto.UserCreateDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
+@Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class UserCreateControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
+    @MockBean
+    private JavaMailSender mailSender;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    
     @Test
-    void 헬스_체크_응답이_200으로_내려온다() throws Exception {
+    void 사용자는_회원가입을_할_수_있고_회원가입된_사용자는_PENDING_상태이다() throws Exception {
         //given
-        mockMvc.perform(MockMvcRequestBuilders.get("/health_check.html"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+                .email("jinsy731@gmail.com")
+                .nickname("jinsy731")
+                .address("Seoul")
+                .build();
+        BDDMockito.doNothing().when(mailSender).send(BDDMockito.any(SimpleMailMessage.class));
         //when
         //then
+        mockMvc.perform(post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userCreateDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.email").value("jinsy731@gmail.com"))
+                .andExpect(jsonPath("$.nickname").value("jinsy731"))
+                .andExpect(jsonPath("$.address").doesNotExist())
+                .andExpect(jsonPath("$.status").value("PENDING"));
     }
 }
